3b7133cb442f8683453dc32a8bc99fa6f2009cf7
PaengBomin <bomin1815@naver.com>
2025-04-09 21:30:20 +0900

    Title: 함수를 이용한 소수 판별, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250409/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// 소수인지 판별하는 함수
+bool isPrime(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+// a~b 사이의 모든 소수의 합 계산
+int sosu(int a, int b) {
+    int sum = 0;
+    for (int i = a; i <= b; i++) {
+        if (isPrime(i)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    cin >> a >> b;
+    cout << sosu(a, b);
+    return 0;
+}
