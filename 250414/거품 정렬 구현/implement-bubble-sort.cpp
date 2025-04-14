25a66c4a91f53ab520770fb07e1d3ea55b32d240
PaengBomin <bomin1815@naver.com>
2025-04-14 12:22:33 +0900

    Title: 거품 정렬 구현, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250414/거품 정렬 구현/implement-bubble-sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int arr[100];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // 거품 정렬
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // 출력
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
