b871c7782a59fdc7d4589780576834e76a64ec68
PaengBomin <bomin1815@naver.com>
2025-06-05 05:17:18 +0900

    Title: 오름 내림차순 정렬, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250605/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int n;
+int nums[100];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    sort(nums,nums+5);
+
+    for (int i=0;i<n;i++){
+        cout << nums[i] << " ";
+    }
+
+    cout << endl;
+
+    sort(nums,nums+5,greater<int>());
+    
+    for (int i=0;i<5;i++){
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
