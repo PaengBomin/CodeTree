9caec3f84befc36469108d43a3f746bb5223917e
PaengBomin <bomin1815@naver.com>
2025-06-05 06:21:46 +0900

    Title: k번째로 신기한 문자열, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250605/k번째로 신기한 문자열/kth-special-string.cpp
+++ 250605/k번째로 신기한 문자열/kth-special-string.cpp
@@ -17,18 +17,17 @@ int main() {
 
     vector<string> newstr;
 
-    sort(str,str+n);
+    sort(str, str + n);
 
-    for (int i=0;i<n;i++) {
-        if (str[i].substr(0,2)=="ap") { 
+    for (int i = 0; i < n; i++) {
+        if (str[i].size() >= t.size() && str[i].substr(0, t.size()) == t) {
             newstr.push_back(str[i]);
         }
     }
 
-
     if (k <= newstr.size()) {
         cout << newstr[k - 1] << endl;
-        }
+    }
 
     return 0;
 }
